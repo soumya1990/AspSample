143446f5426a130ca5cf4812404dff9a7e588cc2
soumyamund <soumyaranjan.nitr@gmail.com>
2023-01-24 20:08:50 +0530

    Init project

---
+++ TestAspApp/TestAspApp/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+var conn = configuration.GetValue<string>("ConnectionStrings");
+
+Console.WriteLine("pwd:"+ conn);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
